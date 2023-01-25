fb1d7ddbfc8d926d7ae990ead93bba43de9c4af8
Jyotiraditya singh Deora <jyotiradityasinghdeora@Jyotiradityas-MacBook-Pro.local>
2023-01-24 21:29:49 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+import datetime
+def my_brand(Assignment_name):
+    print("=*=*=*= Jyotiraditya Singh Deora =*=*=*=")
+    print("=*=*=*= Course 2023S-SSW567-A=*=*=*= ")
+    print("=*=*=*= " + Assignment_name + " =*=*=*= ")
+    print("=*=*=*= " + str(datetime.datetime.now()) + " =*=*=*= ")
+
+my_brand("Tool Setup")
+print("HelloWorld")
+my_brand("Tool Setup")
+
+
+
+
+
+
+
